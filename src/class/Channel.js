e81f092b9442e004781c487e7fc5f4b91261e835
charo164 <jjgcoly164@gmail.com>
2021-08-11 21:13:57 +0000

    refactor: :recycle:

--- src/class/Channel.js
+++ src/class/Channel.js
@@ -64,6 +64,50 @@ class Channel {
       return bot.sendMessage(msg.chat.id, `Merci pour l'anime ${anime.name}`);
     }
     if ('text' in msg) return;
+    if ('photo' in msg) {
+      msgHistory = await bot.sendPhoto(process.env.ANIMO_CHANNEL_ID, msg.photo[0].file_id, {
+        caption: animeName,
+      });
+      return;
+    } else if ('video' in msg) {
+      msgHistory = await bot.sendVideo(process.env.ANIMO_CHANNEL_ID, msg.video.file_id, {
+        caption: animeName,
+      });
+    } else if ('document' in msg) {
+      msgHistory = await bot.sendDocument(process.env.ANIMO_CHANNEL_ID, msg.document.file_id, {
+        caption: animeName,
+      });
+    }
+    if (msgHistory) {
+      const msgString = JSON.stringify(msgHistory);
+      await MsgHistorySchema.create({ msg: msgString });
+    }
+  }
+}
+
+/* let msgHistory = null;
+    if (msg.text && msg.text.startsWith('#newAnime')) {
+      const anime = (await AnimeModel.find().exec())[0];
+      if (anime)
+        return bot.sendMessage(
+          msg.chat.id,
+          `L'anime ${anime.name} est cours de transfère, #fin pour le supprimer`
+        );
+      const name = msg.text.trim().split(' ').slice(1).join(' ');
+      if (!name) return bot.sendMessage(msg.chat.id, 'Syntax invalide ❗️, réessayer.');
+      if (name.length < 4)
+        return bot.sendMessage(msg.chat.id, 'Nom de trop court ❗️, réessayer.');
+      const doc = await AnimeModel.create({ name });
+      bot.sendMessage(msg.chat.id, const_msg.FORWARD_SAVED_MSG(doc.name));
+      return;
+    }
+    if (msg.text && msg.text.startsWith('#fin')) {
+      const anime = (await AnimeModel.find().exec())[0];
+      if (!anime) return;
+      await AnimeModel.findByIdAndRemove(anime._id);
+      return bot.sendMessage(msg.chat.id, `Merci pour l'anime ${anime.name}`);
+    }
+    if ('text' in msg) return;
     const animeName = (await AnimeModel.find().exec())[0]?.name;
     if (!animeName) {
       return bot.sendMessage(
@@ -94,7 +138,6 @@ class Channel {
       const msgString = JSON.stringify(msgHistory);
       await MsgHistorySchema.create({ msg: msgString });
     }
-  }
-}
+  } */
 
 module.exports = Channel;
